b0a689529e61a36d7193e77724f4f3a8cc23fd41
Louis <spalax@gresille.org>
2014-12-22 13:25:41 +0100

    Allow space around length separator

--- pdfautonup/paper.py
+++ pdfautonup/paper.py
@@ -25,64 +25,64 @@ from pdfautonup import errors
 
 PAPERSIZES = {
     # http://www.printernational.org/iso-paper-sizes.php
-    "4a0": "1682mmx2378mm",
-    "2a0": "1189mmx1682mm",
-    "a0": "841mmx1189mm",
-    "a1": "594mmx841mm",
-    "a2": "420mmx594mm",
-    "a3": "297mmx420mm",
-    "a4": "210mm297mm",
-    "a5": "148mmx210mm",
-    "a6": "105mmx148mm",
-    "a7": "74mmx105mm",
-    "a8": "52mmx74mm",
-    "a9": "37mmx52mm",
-    "a10": "26mmx37mm",
+    "4a0": "1682mm x 2378mm",
+    "2a0": "1189mm x 1682mm",
+    "a0": "841mm x 1189mm",
+    "a1": "594mm x 841mm",
+    "a2": "420mm x 594mm",
+    "a3": "297mm x 420mm",
+    "a4": "210mm x 297mm",
+    "a5": "148mm x 210mm",
+    "a6": "105mm x 148mm",
+    "a7": "74mm x 105mm",
+    "a8": "52mm x 74mm",
+    "a9": "37mm x 52mm",
+    "a10": "26mm x 37mm",
 
-    "b0": "1000mmx1414mm",
-    "b1": "707mmx1000mm",
-    "b2": "500mmx707mm",
-    "b3": "353mmx500mm",
-    "b4": "250mmx352mm",
-    "b5": "176mmx250mm",
-    "b6": "125mmx176mm",
-    "b7": "88mmx125mm",
-    "b8": "62mmx88mm",
-    "b9": "44mmx62mm",
-    "b10": "31mmx44mm",
+    "b0": "1000mm x 1414mm",
+    "b1": "707mm x 1000mm",
+    "b2": "500mm x 707mm",
+    "b3": "353mm x 500mm",
+    "b4": "250mm x 352mm",
+    "b5": "176mm x 250mm",
+    "b6": "125mm x 176mm",
+    "b7": "88mm x 125mm",
+    "b8": "62mm x 88mm",
+    "b9": "44mm x 62mm",
+    "b10": "31mm x 44mm",
 
     # http://www.paper-sizes.com/north-american-paper-sizes/north-american-architectural-paper-sizes
-    "archA": "9inx12in",
-    "archB": "12inx18in",
-    "archC": "18inx24in",
-    "archD": "24inx36in",
-    "archE": "36inx48in",
+    "archA": "9in x 12in",
+    "archB": "12in x 18in",
+    "archC": "18in x 24in",
+    "archD": "24in x 36in",
+    "archE": "36in x 48in",
 
     # http://www.engineeringtoolbox.com/office-paper-sizes-d_213.html
-    "letter": "8.5inx11in",
-    "legal": "8.5inx14in",
-    "executive": "7inx10in",
-    "tabloid": "11inx17in",
-    "statement": "5inx8.5in",
-    "halfletter": "5inx8.5in",
-    "folio": "8inx13in",
+    "letter": "8.5in x 11in",
+    "legal": "8.5in x 14in",
+    "executive": "7in x 10in",
+    "tabloid": "11in x 17in",
+    "statement": "5in x 8.5in",
+    "halfletter": "5in x 8.5in",
+    "folio": "8in x 13in",
 
     # http://www.paper-sizes.com/north-american-paper-sizes/north-american-loose-paper-sizes
-    "ledger": "17inx11in",
+    "ledger": "17in x 11in",
 
     # http://simple.wikipedia.org/wiki/Paper_size
-    "quarto": "8inx10in",
+    "quarto": "8in x 10in",
 
     # http://hplipopensource.com/hplip-web/tech_docs/page_sizes.html
-    "flsa": "8.5inx13in",
+    "flsa": "8.5in x 13in",
 
     # http://www.coding-guidelines.com/numbers/ndb/units/area.txt
-    "flse": "8.5inx13in",
+    "flse": "8.5in x 13in",
 
     # http://jexcelapi.sourceforge.net/resources/javadocs/2_6_10/docs/jxl/format/PaperSize.html
-    "note": "8.5inx11in",
-    "11x17": "11inx17in",
-    "10x14": "10inx14in",
+    "note": "8.5in x 11in",
+    "11x17": "11in x 17in",
+    "10x14": "10in x 14in",
 
     }
 
@@ -102,7 +102,7 @@ UNITS = {
 
 UNITS_RE = r"({})".format("|".join(UNITS.keys()))
 SIZE_RE = r"([\d.]+){}".format(UNITS_RE)
-PAPERSIZE_RE = r"^(?P<width>{size})[x× ]?(?P<height>{size})$".format(
+PAPERSIZE_RE = r"^(?P<width>{size}) *[x× ]? *(?P<height>{size})$".format(
     size=SIZE_RE
     )
 
