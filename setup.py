f0b7e8cf67dc34d42677a760e86dd6a4c91862c7
Louis <spalax@gresille.org>
2015-06-09 20:27:45 +0200

    Supporting python3.5

--- setup.py
+++ setup.py
@@ -63,6 +63,8 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Topic :: Printing",
         ],
     long_description=readme(),
