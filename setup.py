a30fd573002ce9f6f245ab04784f27e236e53458
Louis <spalax@gresille.org>
2019-11-08 20:05:31 +0100

    Python3.8 is not supported yet
    Partial revert of dfbb5893

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Topic :: Printing",
     ],
     long_description=readme(),
